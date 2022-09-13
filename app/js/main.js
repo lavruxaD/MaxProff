492def0593bde9d39fc5479c3dc95bcc8a2bd39b
lavruxaD <lavrentevd0@gmail.com>
2022-09-13 14:45:45 +0300

    added price

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,15 @@
 $(function () {
-  $('.header__bottom-item').on('click', function (e) {
+  $(".header__bottom-item").on("click", function (e) {
     e.preventDefault();
-    $($(this).siblings()).removeClass('header__bottom-item--active');
+    $($(this).siblings()).removeClass("header__bottom-item--active");
 
-    $(this).toggleClass('header__bottom-item--active');
+    $(this).toggleClass("header__bottom-item--active");
   });
 
   $(".js-range-slider").ionRangeSlider({
     min: 10,
     max: 320,
     from: 35,
-
-});
+  });
+  
 });
